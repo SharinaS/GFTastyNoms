b8bcd14b78b04b4cc2c9d199c3f24b989d083c10
Sharina Stubbs <sharinastubbs@gmail.com>
2020-02-20 14:13:40 -0800

    set up empty project, updated gitignore and added a second empty class

---
+++ app/src/main/java/com/sharinastubbs/gftastynoms/addRestaurant.java
@@ -0,0 +1,14 @@
+package com.sharinastubbs.gftastynoms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+    }
+}
